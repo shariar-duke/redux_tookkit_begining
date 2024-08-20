cfbeee2dde84c2323270745ce81876a3cb84699d
Shariar Duke <shariarmahmud225@gmail.com>
2024-08-20 10:20:50 +0600

    counterSlice created

--- features/counter/counterSlice.js
+++ features/counter/counterSlice.js
@@ -5,3 +5,23 @@ const createSlice = require("@reduxjs/toolkit")
 const initialState ={
     count:0
 }
+
+// ekhn amra main counterSlice ta banabo
+
+const counterSlice = createSlice({
+    name : "counter",
+    initialState: initialState,
+    reducers:{
+        increment:(state, action) => 
+        {
+            state.count++;
+        },
+
+        decrement:(state, action) => 
+        {
+            state.count --
+        }
+    }
+})
+
+modules.exports = counterSlice.reducers
