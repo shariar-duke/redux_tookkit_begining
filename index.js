cf70b1bed464dd8136f23a663cb57ecca8fb6417
Shariar Duke <shariarmahmud225@gmail.com>
2024-08-20 23:43:01 +0600

    dipatching fetch function from index.js

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 const store = require("./app/store.js") // store tak import kore anlam
 const {counterActions} = require("./features/counter/counterSlice")  
 const { dynamicCounterActions }= require ("./features/dynamicCounter/dynamicCounterSlice.js")
-
+const {fetchPosts} = require("./features/posts/postSlice.js")
 
 store.subscribe(()=> 
 {
     // console.log(store.getState())
 })
 
+
+
+
+
+
+
+// Now I want to dispatch the fetchPosts function 
+
+store.dispatch(fetchPosts())
+
+
+
+
+
 // acitons for dynamicCoutner
 
-store.dispatch(dynamicCounterActions.increment(3)) // ei increment function er modhe perameter
-store.dispatch(dynamicCounterActions.increment(4))
-store.dispatch(dynamicCounterActions.decrement(2))
+// store.dispatch(dynamicCounterActions.increment(3)) // ei increment function er modhe perameter
+// store.dispatch(dynamicCounterActions.increment(4))
+// store.dispatch(dynamicCounterActions.decrement(2))
 
 
 // // actions for coutner
